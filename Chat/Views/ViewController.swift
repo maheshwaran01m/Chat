711398a731e1537d28aa880d1dc18eb56f70b6f5
MAHESHWARAN <maheshwaran@eam360.com>
2024-01-07 08:07:03 +0530

    First commit

---
+++ Chat/Views/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Chat
+//
+//  Created by MAHESHWARAN on 07/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setupView()
+  }
+  
+  private func setupView() {
+    view.backgroundColor = .systemBackground
+    title = "Home"
+  }
+}
